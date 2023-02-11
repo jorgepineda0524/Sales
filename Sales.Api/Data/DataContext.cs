9f15556ad66df6230c381d0fb1f99e2d691c1252
Jorge Pineda <jpineda@pasteur.com.co>
2023-02-11 18:52:34 -0500

    Add datacontext

---
+++ Sales.Api/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(o => o.Name).IsUnique();
+        }
+    }
+}
