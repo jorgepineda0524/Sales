1d965b90071bfb75172c5532f0d4063eba2bea2f
Jorge Pineda <jpineda@pasteur.com.co>
2023-02-22 22:58:21 -0500

    add and validation of form Country

--- Sales.Web/Repositories/Repository.cs
+++ Sales.Web/Repositories/Repository.cs
@@ -55,5 +55,32 @@ namespace Sales.Web.Repositories
             var respuestaString = await httpResponse.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<T>(respuestaString, jsonSerializerOptions)!;
         }
+
+        public async Task<HttpResponseWrapper<object>> Delete(string url)
+        {
+            var responseHTTP = await _httpClient.DeleteAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
+        }
+
+        public async Task<HttpResponseWrapper<object>> Put<T>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHTTP = await _httpClient.PutAsync(url,messageContent);
+            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
+        }
+
+        public async Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContent = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHTTP = await _httpClient.PutAsync(url, messageContent);
+            if (responseHTTP.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswer<TResponse>(responseHTTP, _jsonDefaultOptions);
+                return new HttpResponseWrapper<TResponse>(response, false, responseHTTP);
+            }
+            return new HttpResponseWrapper<TResponse>(default, !responseHTTP.IsSuccessStatusCode, responseHTTP);
+        }
     }
 }
