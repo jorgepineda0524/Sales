6c880e139095dc6dc95bc5e90c788546177700dc
Jorge Pineda <jpineda@pasteur.com.co>
2023-02-25 17:38:51 -0500

    seed db

--- Sales.Api/Program.cs
+++ Sales.Api/Program.cs
@@ -11,6 +11,20 @@ builder.Services.AddTransient<SeedDb>();
 
 var app = builder.Build();
 
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
