26cef032920d4ba4c3f722a294ad625e5939d97a
Jorge Pineda <jpineda@pasteur.com.co>
2023-03-05 10:01:20 -0500

    api countries comunity

--- Sales.Api/Data/SeedDb.cs
+++ Sales.Api/Data/SeedDb.cs
@@ -1,14 +1,19 @@
-﻿using Sales.Shared.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Api.Services;
+using Sales.Shared.Entities;
+using Sales.Shared.Responses;
 
 namespace Sales.Api.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
+        private readonly IApiService _apiService;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IApiService apiService)
         {
             _context = context;
+            _apiService = apiService;
         }
 
         public async Task SeedAsync()
@@ -21,65 +26,58 @@ namespace Sales.Api.Data
         {
             if (!_context.Countries.Any())
             {
-                _context.Countries.Add(new Country 
-                { 
-                    Name = "Colombia" ,
-                    States = new List<State>
-                    {
-                        new State 
-                        { 
-                            Name = "Antioquia",
-                            Cities = new List<City>
-                            {
-                                new City { Name = "Medellín" },
-                                new City { Name = "Itagüi" },
-                                new City { Name = "Sabaneta" }
-                            }
-                        },
-                        new State
-                        {
-                            Name = "Bogotá",
-                            Cities = new List<City>
-                            {
-                                new City { Name = "Usaquen" },
-                                new City { Name = "Chapinero" },
-                                new City { Name = "Bosa" }
-                            }
-                        },
-                    }
-                });
-                _context.Countries.Add(new Country
+                Response responseCountries = await _apiService.GetListAsync<CountryResponse>("/v1", "/countries");
+                if (responseCountries.IsSuccess)
                 {
-                    Name = "Estados Unidos",
-                    States = new List<State>
+                    List<CountryResponse> countries = (List<CountryResponse>)responseCountries.Result!;
+                    foreach (CountryResponse countryResponse in countries)
                     {
-                        new State
+                        Country country = await _context.Countries!.FirstOrDefaultAsync(c => c.Name == countryResponse.Name!)!;
+                        if (country == null)
                         {
-                            Name = "Florida",
-                            Cities = new List<City>
+                            country = new() { Name = countryResponse.Name!, States = new List<State>() };
+                            Response responseStates = await _apiService.GetListAsync<StateResponse>("/v1", $"/countries/{countryResponse.Iso2}/states");
+                            if (responseStates.IsSuccess)
                             {
-                                new City { Name = "Orlando" },
-                                new City { Name = "Miami" },
-                                new City { Name = "Tampa" }
+                                List<StateResponse> states = (List<StateResponse>)responseStates.Result!;
+                                foreach (StateResponse stateResponse in states!)
+                                {
+                                    State state = country.States!.FirstOrDefault(s => s.Name == stateResponse.Name!)!;
+                                    if (state == null)
+                                    {
+                                        state = new() { Name = stateResponse.Name!, Cities = new List<City>() };
+                                        Response responseCities = await _apiService.GetListAsync<CityResponse>("/v1", $"/countries/{countryResponse.Iso2}/states/{stateResponse.Iso2}/cities");
+                                        if (responseCities.IsSuccess)
+                                        {
+                                            List<CityResponse> cities = (List<CityResponse>)responseCities.Result!;
+                                            foreach (CityResponse cityResponse in cities)
+                                            {
+                                                if (cityResponse.Name == "Mosfellsbær" || cityResponse.Name == "Șăulița")
+                                                {
+                                                    continue;
+                                                }
+                                                City city = state.Cities!.FirstOrDefault(c => c.Name == cityResponse.Name!)!;
+                                                if (city == null)
+                                                {
+                                                    state.Cities.Add(new City() { Name = cityResponse.Name! });
+                                                }
+                                            }
+                                        }
+                                        if (state.CitiesNumber > 0)
+                                        {
+                                            country.States.Add(state);
+                                        }
+                                    }
+                                }
                             }
-                        },
-                        new State
-                        {
-                            Name = "Texas",
-                            Cities = new List<City>
+                            if (country.StatesNumber > 0)
                             {
-                                new City { Name = "Houston" },
-                                new City { Name = "San Antionio" },
-                                new City { Name = "Dallas" }
+                                _context.Countries.Add(country);
+                                await _context.SaveChangesAsync();
                             }
-                        },
+                        }
                     }
-                });
-
-                _context.Categories.Add(new Category { Name = "Moda" });
-                _context.Categories.Add(new Category { Name = "Calzado" });
-                _context.Categories.Add(new Category { Name = "Tecnología" });
-                await _context.SaveChangesAsync();
+                }
             }
         }
     }
---
+++ Sales.Api/Services/IApiService.cs
@@ -0,0 +1,9 @@
+﻿using Sales.Shared.Responses;
+
+namespace Sales.Api.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string serviciePrefix, string controller);
+    }
+}
