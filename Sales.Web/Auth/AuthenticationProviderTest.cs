a628d6120ca5517e4a429db1132d582c24b214f0
Jorge Pineda <jpineda@pasteur.com.co>
2023-03-21 21:54:00 -0500

    security hardcore on web

---
+++ Sales.Web/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.Web.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimus = new ClaimsIdentity();
+            var jorge = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName","Juan"),
+                new Claim("LastName", "Zulu"),
+                new Claim(ClaimTypes.Name, "zulu@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimus)));
+        }
+    }
+}
