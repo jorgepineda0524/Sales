536251e08a6b63b91714148bf31f02838025fa94
Jorge Pineda <jpineda@pasteur.com.co>
2023-03-26 22:34:17 -0500

    add user photo

--- Sales.Api/Program.cs
+++ Sales.Api/Program.cs
@@ -51,6 +51,7 @@ builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConn
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped<IApiService, ApiService>();
 builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddScoped<IFileStorage, FileStorage>();
 
 builder.Services.AddIdentity<User, IdentityRole>(x =>
 {
