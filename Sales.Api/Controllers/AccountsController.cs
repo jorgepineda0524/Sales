75dcd6ab1006ffb9030855e9936128e30c0340ab
Jorge Pineda <jpineda@pasteur.com.co>
2023-03-28 21:48:51 -0500

    edit user fixes

--- Sales.Api/Controllers/AccountsController.cs
+++ Sales.Api/Controllers/AccountsController.cs
@@ -83,7 +83,7 @@ namespace Sales.Api.Controllers
                 var result = await _userHelper.UpdateUserAsync(currentUser);
                 if (result.Succeeded)
                 {
-                    return NoContent();
+                    return Ok(BuildToken(currentUser));
                 }
 
                 return BadRequest(result.Errors.FirstOrDefault());
